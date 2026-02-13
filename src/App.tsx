98eefd59442866d9eb83883829d914250e5bc468
Hardhik Annam <hannam@udel.edu>
2026-02-13 16:10:46 -0500

    Update App layout/styles for HTML/CSS task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,12 +8,52 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <p>
-                Hardhik Annam
-            </p>
-            <p>
-                Hello World
-            </p>
+            <p>Hardhik Annam</p>
+            <p>Hello World</p>
+
+            <img
+                src="../assets/images/mountainbackground.jpg"
+                alt="Picture of a mountain"
+            />
+
+            <h1>Website Version 1.0</h1>
+
+            <ul>
+                <li>Apples</li>
+                <li>Mangoes</li>
+                <li>Strawberries</li>
+            </ul>
+
+            <Button
+                onClick={() => {
+                    console.log("Hello World!");
+                }}
+            >
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+
+                    <Col>
+                        <div
+                            style={{
+                                width: "100px",
+                                height: "100px",
+                                backgroundColor: "red",
+                            }}
+                        />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
